23af264f6dce2412b364d03bff5aaabf592828c4
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-27 10:30:50 +0200

    Snake class

--- main.py
+++ main.py
@@ -16,7 +16,18 @@ BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
-    pass
+
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 class Food:
 
@@ -28,7 +39,6 @@ class Food:
         self.coordinates = [x, y]
 
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="jedzonko")
-
 
 def next_turn():
     pass
