e0d7ee73024ba16c6c3d3ccf2f607d76a8699d40
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-27 10:08:49 +0200

    Window game parameters and colours

--- main.py
+++ main.py
@@ -5,6 +5,16 @@
 from tkinter import *
 import random
 
+GAME_WIDTH = 1000
+GAME_HEIGHT = 1000
+SPEED = 65
+SPACE_SIZE = 50
+BODY_PARTS = 2
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
 class Snake:
     pass
 
@@ -14,7 +24,7 @@ class Food:
 def next_turn():
     pass
 
-def change_direction():
+def change_direction(new_direction):
     pass
 
 def check_collisions():
