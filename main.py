89b022e3444d10edf1bd469bc19ea7d44c4f1a61
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-04-27 10:19:26 +0200

    Window game with score

--- main.py
+++ main.py
@@ -43,4 +43,7 @@ direction = 'down'
 label = Label(window, text="Wynik:{}".format(score), font=('consolas', 40))
 label.pack()
 
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
 window.mainloop()
